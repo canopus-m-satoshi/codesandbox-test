696fd2e4dc70b4fef2f204a5e1cc08e6172a1ba2
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-03-27 23:38:47 +0900

    デフォルト値 引数など

--- src/index.js
+++ src/index.js
@@ -19,16 +19,22 @@
 
 // 分割代入
 
-const myProfile = {
-  name: "Sato",
-  age: 27
-};
+// const myProfile = {
+//   name: "Sato",
+//   age: 27
+// };
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です`;
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です`;
 
-console.log(message1);
+// console.log(message1);
 
-// 分割代入を設定
-const { name, age } = myProfile;
-const message2 = `名前は${name}です。年齢は${age}です`;
-console.log(message2);
+// // 分割代入を設定
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+// デフォルト値 引数など
+const sayHelllo = (name = "Will") => console.log(`Hello, ${name}!`);
+
+sayHelllo("Taro");
+sayHelllo();
