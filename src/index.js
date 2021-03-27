716c144975782b0d67e6360cfa0389125842d62f
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-03-27 23:17:36 +0900

    arrow function practice

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@
 // var val1 = "var various";
 // console.log(val1);
 
-const val5 = ["dog", "cat"];
-val5.push("monkey");
-val5.push("aaa");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5.push("monkey");
+// val5.push("aaa");
+// console.log(val5);
+
+// arrow function
+
+const plus = (num1, num2) => num1 + num2;
+
+console.log(plus(1, 20));
