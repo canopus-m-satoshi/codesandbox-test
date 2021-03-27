40047ecddbdcd5b6a9fbd2cf51f2b3ae55fc542a
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-03-28 00:22:39 +0900

    mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -60,11 +60,46 @@
 // console.log(arry3);
 
 // 配列のコピーや結合
-const arry4 = [10, 20];
-const arry5 = [30, 40];
+// const arry4 = [10, 20];
+// const arry5 = [30, 40];
 
-const arry6 = [...arry4];
-console.log(arry6); //  [10, 20]
+// const arry6 = [...arry4];
+// console.log(arry6); //  [10, 20]
 
-const arry7 = [...arry4, ...arry5];
-console.log(arry7); //   [10, 20, 30, 40]
+// const arry7 = [...arry4, ...arry5];
+// console.log(arry7); //   [10, 20, 30, 40]
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+const nameArry1 = ["Mike", "Will", "Dustin", "Lucas"];
+
+// // map() 配列に対して使用できる関数
+// const nameArry2 = nameArry1.map((name) => {
+//   return name;
+// });
+
+// console.log(nameArry2); // ["Mike", "Will", "Dustin", "Lucas"]
+
+// nameArry1.map((name) => console.log(name)); //Mike Will Dustin Lucas
+
+// filter() ある条件に一致した値のみ返す関数
+// const numArry1 = [1, 2, 3, 4, 5];
+// const newNumArry = numArry1.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArry); //  [1, 3, 5]
+
+// ~は何番目です　としたい時
+// nameArry1.map((name, index) => console.log(`${name}は${index + 1}番目です`)); //Mikeは1番目です Willは2番目です Dustinは3番目です Lucasは4番目です
+
+const newNameArry = nameArry1.map((name) => {
+  if (name === "Mike") {
+    return name;
+  } else {
+    return `Mr ${name}`;
+  }
+});
+
+console.log(newNameArry); // ["Mike", "Mr Will", "Mr Dustin", "Mr Lucas"]
